338aad8697b4fa552748cd2f0b9549a93d3fd64e
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-03-07 21:26:45 +0100

    zadanie z iterowaniem

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -1,1 +1,18 @@
-vhhfgfh
+liczby=[1,2,3,4,6,7,8,-8,-9,10]
+i=0
+for x in liczby:
+    print(x)
+    if x<0:
+        i=i+1
+print(i,"ilość liczb ujemnych ")
+
+#to samo na while
+
+# d=len(liczby)
+# n=0
+# while n<d:
+#     print(liczby[n])
+#     if liczby[n]<0:
+#         i=i+1
+#     n=n+1
+# print(i,"ilość liczb ujemnych ")
