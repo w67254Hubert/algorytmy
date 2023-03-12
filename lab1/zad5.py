0f75b952812450ae15ec685aa1ec90156898578b
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-03-12 14:14:17 +0100

    zadanie 5 zrobione

--- lab1/zad5.py
+++ lab1/zad5.py
@@ -1,18 +1,25 @@
-import numpy as np
+#pierwsza myśl znajduje minimalną ale nie mam pomysłu jak zaimplementować zmianę wartości
+'''import numpy as np
 
+lista=np.array([[0,-2,0,4,3,5,],[6,7,-9,8,-2,9]])
 
-lista= np.array([[0,2,0,4,3,5,],[6,7,-9,8,-2,9]])
 i=0
-min=lista[0,0]
-min2=lista[1,0]
 for x in lista:
-    print(x)
     for y in x:
-        print(y)
         if y<lista[i,0]:
             lista[i,0]=y
     i=i+1   
 
-print(lista[0,0])
-
 print(lista[1,0])
+print(lista[0,0])
+'''
+# drugie podejście po edukacji że jednak nie potrzebuje nupmy by stworzyć tablice wielowymiarową
+
+lista = [[0,-2,0,4,3,5,],[6,7,-9,8,-2,9]]
+
+for x in lista:
+    minim = min(x) 
+    i = x.index(minim)
+    x[0], x[i] = x[i], x[0]
+
+print(lista)
