ce0d55e1b812e606d6120efbf862a335b0c8c036
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-03-12 12:48:55 +0100

    to chyba nie to co trzeba zrobić

--- lab1/zad5.py
+++ lab1/zad5.py
@@ -2,14 +2,17 @@ import numpy as np
 
 
 lista= np.array([[0,2,0,4,3,5,],[6,7,-9,8,-2,9]])
-
+i=0
 min=lista[0,0]
 min2=lista[1,0]
 for x in lista:
     print(x)
     for y in x:
         print(y)
-        if y<lista[0,0]:
-            min=y
+        if y<lista[i,0]:
+            lista[i,0]=y
+    i=i+1   
 
-print(min)
+print(lista[0,0])
+
+print(lista[1,0])
