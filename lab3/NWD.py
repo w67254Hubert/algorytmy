28099445ee1a6f8e496ee33e8b46b9d8a1d7f9d4
w67254Hubert <w67254@student.wsiz.edu.pl>
2023-03-24 13:34:56 +0100

    zadania do 5

--- lab3/NWD.py
+++ lab3/NWD.py
@@ -32,4 +32,38 @@ def nwdrekII(a,b):
     if b!=0:
         return nwdrekII(b,a%b)
     return a
+##wersja pierwsza nwdIteracyjne
+def nwdIter(a,b):
+    while a!=b:
+        if a>b: a=a-b
+        else: b=b-a
+    return a
+
+print(nwdIter(12,18))
+print(nwdIter(28,24))
+
+# wersja  rekurencyjnie
+def nwdRek(a,b):
+    if a!=b:
+        if a>b: return nwdRek(a-b,b) #a=a-b
+        else: return nwdRek(a,b-a) #b=b-a
+    return a
+
+print(nwdRek(12,18))
+
+# 2 wersja iterowanego
+
+def nwditerII(a,b):
+    while b!=0:
+        temp=b
+        b=a%b
+        a=temp
+    return a
+
+# 2 wersja rekurencyjnego
+
+def nwdrekII(a,b):
+    if b!=0:
+        return nwdrekII(b,a%b)
+    return a
 #
--- lab3/zadanie5.py
+++ lab3/zadanie5.py
@@ -1,14 +1,9 @@
 
 def f(n):
-    while n>=1:
-        b=1/1-f(n-1)
-    n=n-1
-    
+    if n==1:
+        return 4
+    else: return 1/(1-f(n-1))
 
 
-n=0
-a=4
-f(n,a)
-
-
-#nie wiem jak narazie zrobić zadanie z funkcją
+n=100
+print(f(n))
